cfaddedb847a0f5ecdfa211090f97e8fd560dd8b
ShadowOfNight7 <lukeleeau@gmail.com>
2026-08-15 18:24:58 +1000

    Removed /plugins/extracted-plugins from my git exclude for QoL

---
+++ plugins/extracted-plugins/[DEPRECATED]ScopedCrossbow/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+}
+
+group = "com.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(21)
+    }
+}
